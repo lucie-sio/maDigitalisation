c11a747d56e4bd4133be516bc15ef5a4934cf47e
Evil Lucie <roulier.lucie@outlook.fr>
2023-04-24 16:33:37 +0200

    fix images + path

--- components/navbar.php
+++ components/navbar.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-body-secondary">
   <div class="container-fluid">
-    <img src="/assets/logo.png" alt="Digitalisation" height="50" width="50">
+    <img src="assets/logo.png" alt="Digitalisation" height="50" width="50">
     <div class="navbar-collapse">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
@@ -11,9 +11,9 @@
             Axes
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="/competence.php">Compétences</a></li>
-            <li><a class="dropdown-item" href="/reactivite.php">Réactivité</a></li>
-            <li><a class="dropdown-item" href="/numerique.php">Numérique</a></li>
+            <li><a class="dropdown-item" href="competence.php">Compétences</a></li>
+            <li><a class="dropdown-item" href="reactivite.php">Réactivité</a></li>
+            <li><a class="dropdown-item" href="numerique.php">Numérique</a></li>
           </ul>
         </li>
       </ul>
