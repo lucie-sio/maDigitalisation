3c7017174fefa054930ec5f7b10c059baf701dc8
Evil Lucie <roulier.lucie@outlook.fr>
2023-04-25 16:20:52 +0200

    fix doublon

--- company.php
+++ company.php
@@ -38,11 +38,6 @@ include('components/navbar.php'); ?>
                         <tr>
                             <td scope="col" colspan="3" class="text-center"><h1><?= $var['axe_name'] ?></h1></td>
                         </tr>
-                        <tr>
-                            <td><?= $var['item_name'] ?></td>
-                            <td><?= $var['question'] ?></td>
-                            <td><?= $var['score'] ?></td>
-                        </tr>
                     <?php endif; ?>
                     <tr>
                         <td><?= $var['item_name'] ?></td>
