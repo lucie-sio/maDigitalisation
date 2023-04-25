196dcfff0f4436d446b67b59ead4d23caccdd2a7
Evil Lucie <roulier.lucie@outlook.fr>
2023-04-25 11:01:15 +0200

    BDD new version

--- scripts/axes.sql
+++ scripts/axes.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Apr 25, 2023 at 08:09 AM
+-- Generation Time: Apr 25, 2023 at 09:00 AM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -104,6 +104,52 @@ CREATE TABLE `question` (
   `max_score` int DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `question`
+--
+
+INSERT INTO `question` (`id`, `item_id`, `question`, `max_score`) VALUES
+(1, 1, 'Formations pour Apprendre à apprendre, changement de paradigme, \"être à la page\" (au-delà des compétences \"justes\" nécessaires)', 2),
+(2, 1, 'Le co-développement (outil d\'intelligence collective) existe-t-il dans l\'entreprise ?', 2),
+(3, 1, 'Les collaborateurs sont-ils amenés à partager leurs compétences et sous quelles formes ?', 2),
+(4, 1, 'Le mentoring (fonctionnement en binôme) existe-t-il pour assurer le transfert de compétences ?', 2),
+(5, 1, 'Les managers sont-ils aussi formateurs sur certains sujet pour l\'entreprise entière ?', 2),
+(6, 1, 'L\'entreprise favorise-t-elle l\'excellence technique ? (Principe 9 du Manifeste Agile)', 2),
+(7, 2, 'Déployez vous les pratiques du \'Faire Agile\', c\'est-à-dire une ou plusieurs des \'méthodes\' agiles ?', 2),
+(8, 2, 'Votre entreprise promeut-elle un \'état d\'esprit agile\' ?', 2),
+(9, 3, 'Votre entreprise gère-t-elle humainement les compétences ?', 2),
+(10, 4, 'Valeur supérieure utilisable livrée plus tôt (Fonction principale utilisable dès les premières versions)', 2),
+(11, 4, 'Réactivité face aux impératifs prépondérants', 2),
+(12, 4, 'Mesure de la vélocité de l\'équipe (indicateur de suivi du travail d\'une équipe de conception)', 2),
+(13, 5, 'Les installations techniques et de gestion sont modernes (TIC/ERP/CRM)', 2),
+(14, 5, 'Les équipes sont en capacité d\'autonomiser une partie de leurs tâches', 2),
+(15, 5, 'Les équipes s’inscrivent dans un cadre Agile Lean', 2),
+(16, 5, 'Les mécanismes de livraison et de synchronisation sont matures', 2),
+(17, 6, 'Les innovations produit tiennent compte de l\'urgence climatique', 2),
+(18, 6, 'Les processus internes sont remis en cause pour diminuer leur impact environnemental', 2),
+(19, 6, 'Les parties prenantes sont sélectionnées en fonction de leur éthique vis-à-vis du développement durable', 2),
+(20, 7, 'Veille stratégique', 2),
+(21, 8, 'Votre entreprise dégage t-elle une part de CA  par des produits ou services en ligne', 2),
+(22, 8, 'La mise en place d’outils numériques a-t-elle permis d\'optimiser les coûts, les délais et la qualité ?', 2),
+(23, 8, 'Comment les outils sont-ils intégrés dans les process de l’entreprise ?', 2),
+(24, 8, 'Comment partagez-vous les données numériques avec les parties prenantes (clients, fournisseurs,…) ?', 2),
+(25, 8, 'Mesurez-vous les impacts du numérique sur votre entreprise ?', 2),
+(26, 8, 'Quel est l’impact du numérique sur la démarche RSE de l’entreprise ?', 2),
+(27, 8, 'Existe-t-il des freins (stratégiques, financiers,…) à l’ investissement dans les outils numériques ?', 2),
+(28, 9, 'L’entreprise dispose-t-elle : d’un site internet, d’un compte LinkedIn, d’une page Facebook, d’un compte Twitter,... ?', 2),
+(29, 9, 'Comment utilisez-vous le numérique dans la relation client ? (nouveau modèle de vente, nouveau modèle d’échanges avec les clients, communauté, story, chat,...)', 2),
+(30, 9, 'L’entreprise dispose-t-elle d’un community manager ?', 2),
+(31, 9, 'Certains de vos collaborateurs sont-ils actifs sur les réseaux sociaux au nom de l’entreprise ?', 2),
+(32, 9, 'Comment mesurez-vous et exploitez-vous les données issues du site de votre entreprise ?', 2),
+(33, 9, 'Avez-vous observé chez vos concurrents des pratiques innovantes ?', 2),
+(34, 10, 'Vos collaborateurs sont-ils équipés de nouveaux équipements numériques de travail (PC portable, tablette, smartphone,…) ? ', 2),
+(35, 10, 'L’arrivée des outils digitaux a-t-elle eu un impact sur les pratiques et la culture de l’entreprise ?', 2),
+(36, 10, 'Comment vous êtes-vous approprié et comment avez-vous accompagné ces évolutions?', 2),
+(37, 10, 'Les nouvelles possibilités technologiques ont-elles fait évoluer le modèle d’organisation de l’entreprise et/ou votre management, vers plus de collaboration avec des acteurs internes ou externes ?', 2),
+(38, 10, 'Mobilisez-vous des outils de veille et mettez-vous en œuvre des modalités de curation et de partage de l’ information ?', 2),
+(39, 10, 'Les fonctionnalités des outils sont-elles augmentées par la pratique de vos collaborateurs ?', 2),
+(40, 10, 'Comment accompagnez-vous vos collaborateurs dans la transition numérique ?', 2);
+
 -- --------------------------------------------------------
 
 --
@@ -181,7 +227,7 @@ ALTER TABLE `item`
 -- AUTO_INCREMENT for table `question`
 --
 ALTER TABLE `question`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
 
 --
 -- AUTO_INCREMENT for table `score`
